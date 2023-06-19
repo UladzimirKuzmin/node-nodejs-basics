2119f44b18a5949d0bfb04dc700fdfcec7d6d1ca
vladimirk <specialkv@gmail.com>
2023-06-19 19:46:43 +0200

    feat: worker thread scripts

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import os from 'node:os';
+import { Worker } from 'node:worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCores = os.cpus();
+  const threads = cpuCores.map(
+    (_, i) =>
+      new Promise((resolve, reject) => {
+        const worker = new Worker('./worker.js', {
+          workerData: i + 10,
+        });
+
+        worker.on('message', resolve);
+        worker.on('error', () => reject({ status: 'error', data: null }));
+      })
+  );
+
+  const result = await Promise.all(threads);
+
+  console.log(result);
 };
 
 await performCalculations();
